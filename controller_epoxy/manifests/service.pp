5cb58a1d6b4c16aea93e4a6a661a59c3e7ef561c
fanzago <federica.fanzago@cern.ch>
2026-03-13 14:07:26 +0100

    finale

--- controller_epoxy/manifests/service.pp
+++ controller_epoxy/manifests/service.pp
@@ -1,6 +1,6 @@
 class controller_epoxy::service inherits controller_epoxy::params {
   
- ## Services prova 
+ ## Services finale 
 
  service { "memcached":
                    ensure      => stopped,
