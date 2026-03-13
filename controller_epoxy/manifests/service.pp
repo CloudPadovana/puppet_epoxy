255b6c65a393f00447dae5ce628066a885334ce9
fanzago <federica.fanzago@cern.ch>
2026-03-13 12:12:26 +0100

    stop services during update

--- controller_epoxy/manifests/service.pp
+++ controller_epoxy/manifests/service.pp
@@ -3,8 +3,8 @@ class controller_epoxy::service inherits controller_epoxy::params {
  ## Services
 
  service { "memcached":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_horizon'],
@@ -23,8 +23,8 @@ file { "/etc/cron.d/fetch-crl":
 
  # Services for keystone, placement       
     service { "httpd":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => [ Class['controller_epoxy::configure_keystone'], Class['controller_epoxy::configure_horizon'], Class['controller_epoxy::configure_placement'], ],
@@ -32,8 +32,8 @@ file { "/etc/cron.d/fetch-crl":
 
  # Services for Shibboleth
     service { "shibd":
-                   ensure     => running,
-                   enable     => true,
+                   ensure     => stopped,
+                   enable     => false,
                    hasstatus  => true,
                    hasrestart => true,
                    subscribe  => Class['controller_epoxy::configure_shibboleth'],
@@ -41,8 +41,8 @@ file { "/etc/cron.d/fetch-crl":
 
  # Services for Glance
     service { "openstack-glance-api":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_glance'],
@@ -50,90 +50,74 @@ file { "/etc/cron.d/fetch-crl":
 
  # Services for nova       
     service { "openstack-nova-api":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_nova'],
            }
     service { "openstack-nova-scheduler":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_nova'],
            }
     service { "openstack-nova-novncproxy":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_nova'],
            }
     service { "openstack-nova-conductor":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_nova'],
            }
  
 
-# # Services for ec2       
-#    service { "openstack-ec2-api":
-#                   ensure      => running,
-#                   enable      => true,
-#                   hasstatus   => true,
-#                   hasrestart  => true,
-#                   subscribe   => Class['controller_epoxy::configure_ec2'],
-#           }
-#    service { "openstack-ec2-api-metadata":
-#                   ensure      => running,
-#                   enable      => true,
-#                   hasstatus   => true,
-#                   hasrestart  => true,
-#                   subscribe   => Class['controller_epoxy::configure_ec2'],
-#           }
-
  # Services for neutron       
     service { "openvswitch":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_neutron'],
            }
     service { "neutron-server":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_neutron'],
            }
     service { "neutron-openvswitch-agent":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_neutron'],
            }
     service { "neutron-dhcp-agent":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_neutron'],
            }
     service { "neutron-metadata-agent":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_neutron'],
            }
     service { "neutron-l3-agent":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_neutron'],
@@ -141,22 +125,22 @@ file { "/etc/cron.d/fetch-crl":
 
  # Services for cinder
     service { "openstack-cinder-api":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_cinder'],
            }
     service { "openstack-cinder-scheduler":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_cinder'],
            }
     service { "openstack-cinder-volume":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_cinder'],
@@ -164,22 +148,22 @@ file { "/etc/cron.d/fetch-crl":
            
  # Services for heat
     service { "openstack-heat-api":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_heat'],
            }
     service { "openstack-heat-api-cfn":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_heat'],
            }
     service { "openstack-heat-engine":
-                   ensure      => running,
-                   enable      => true,
+                   ensure      => stopped,
+                   enable      => false,
                    hasstatus   => true,
                    hasrestart  => true,
                    subscribe   => Class['controller_epoxy::configure_heat'],
